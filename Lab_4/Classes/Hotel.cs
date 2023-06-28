35c45d2a163662f385667cdbab74259d6a7466c3
Noah <noseby@live.ca>
2023-06-27 23:44:35 -0500

    Lab Complete

---
+++ Lab_4/Classes/Hotel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab_4
+{
+    public class Hotel
+    {
+        public string Name { get; }
+        public string Address { get; }
+        public List<Room> Rooms { get; }
+
+        public List<Client> Clients { get; }
+
+        public Hotel(string name, string address)
+        {
+            Name = name;
+            Address = address;
+            Rooms = new List<Room>();
+            Clients = new List<Client>();
+        }
+    }
+
+}
---
+++ Lab_4/Classes/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab_4
+{
+    public class Room
+    {
+        public string Number { get; }
+        public int Capacity { get; }
+        public bool Occupied { get; }
+        public List<Reservation> Reservations { get; }
+
+        public Room(string number, int capacity)
+        {
+            Number = number;
+            Capacity = capacity;
+            Occupied = false;
+            Reservations = new List<Reservation>();
+        }
+    }
+
+}
